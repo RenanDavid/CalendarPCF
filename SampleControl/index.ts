738b4cc1013c04b1e5b5710739a2743a34794ec1
Moreira, Renan <rmoreir2@its.jnj.com>
2024-07-13 19:13:48 -0300

    Feature: Calendar DOM and Full Calendar Dependencies

--- SampleControl/index.ts
+++ SampleControl/index.ts
@@ -1,53 +1,44 @@
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
+import * as FullCalendar from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 export class SampleControl implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+    private container: HTMLDivElement;
 
-    /**
-     * Empty constructor.
-     */
-    constructor()
-    {
-
+    constructor() {
+        // empty constructor
     }
 
-    /**
-     * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
-     * Data-set values are not initialized here, use updateView.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
-     * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
-     * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
-     * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
-     */
-    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement): void
-    {
-        // Add control initialization code
+    public init(
+        context: ComponentFramework.Context<IInputs>,
+        notifyOutputChanged: () => void,
+        state: ComponentFramework.Dictionary,
+        container: HTMLDivElement
+    ) {
+        this.container = document.createElement("div");
+        container.appendChild(this.container);
+
+        let calendarEl = document.createElement("div");
+        this.container.appendChild(calendarEl);
+
+        let calendar = new FullCalendar.Calendar(calendarEl, {
+            plugins: [dayGridPlugin],
+            initialView: 'dayGridMonth',
+            events: [] // Eventos serão adicionados posteriormente
+        });
+
+        calendar.render();
     }
 
-
-    /**
-     * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
-     */
-    public updateView(context: ComponentFramework.Context<IInputs>): void
-    {
-        // Add code to update control view
+    public updateView(context: ComponentFramework.Context<IInputs>): void {
+        // Código para atualizar a visualização
     }
 
-    /**
-     * It is called by the framework prior to a control receiving new data.
-     * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as "bound" or "output"
-     */
-    public getOutputs(): IOutputs
-    {
+    public getOutputs(): IOutputs {
         return {};
     }
 
-    /**
-     * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
-     * i.e. cancelling any pending remote calls, removing listeners, etc.
-     */
-    public destroy(): void
-    {
-        // Add code to cleanup control if necessary
+    public destroy(): void {
+        // Código para limpar recursos
     }
 }
